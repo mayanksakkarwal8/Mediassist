711fd858001f193d81b70a0f5be4e41a94a0f022
mayanksakkarwal8 <mayanksakkarwal8@gmail.com>
2024-11-07 01:28:21 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () => {
+	return (
+		<footer className='flex mt-12 mb-5 pt-12 justify-center items-center content-end'>
+			<div>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
